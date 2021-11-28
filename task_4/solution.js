32abaa09ee3c3525a3edacf5401467340749b885
Irina Pozdnyakova <Irinka_21_11_93@mail.ru>
2021-11-28 15:56:43 +0300

    task_4 done v2

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,11 +1,15 @@
 // Задание 4.1. Вывод карточек товаров
-
 function parseProducts(json) {
+    let data = JSON.parse(json);
+    let products = data.products;
+        return products;
+}
+function renderProductsCards(json){
     clearProducts();
-    products = parseProducts(json);
-    
+    let products = parseProducts(json);
+    length = products.length;
     for (let i = 0; i < length; i += 1) {
-        addProduct(products[i]); 
+        addProduct(products[i]);
     }  
 }
 // Аргументом функции является JSON
